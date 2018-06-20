4699e4ec8b8311fd0e6307fc7386eee8cbeb24f5
Joshua <hmlu36@gmail.com>
2018-06-20 17:48:20 +0800

    commit

--- js/main.js
+++ js/main.js
@@ -24,8 +24,56 @@ $(function() {
     $(window).scroll(function() {
         if ($(".navbar").offset().top > 10) {
             $(".fixed-top").addClass("top-nav-collapse");
+            $("body").css("background-image", "none");
         } else {
             $(".fixed-top").removeClass("top-nav-collapse");
+            $("body").css("background-image", "url(img/index/background.jpg)");
         }
     });
+
+    var width = $(window).width();
+    var height = $(window).height();
+    //console.log(width);
+
+    var topParameter = $(".navbar-nav").css('top').replace("px", "");
+    topParameter = (topParameter == "auto") ? "180" : topParameter + 50;
+    // console.log(topParameter);
+
+    // Smooth scrolling using jQuery easing
+    $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
+        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+
+            if (target.length) {
+                $('html, body').animate({
+                    scrollTop: (target.offset().top - topParameter)
+                }, 1000, "easeInOutExpo");
+                return false;
+            }
+        }
+    });
+
+    $("a.js-scroll-trigger").click(function() {
+        $(".navbar").removeClass("bg-dark");
+        $('.navbar-collapse').collapse('hide');
+    });
+
+    // Activate scrollspy to add active class to navbar items on scroll
+    $('body').scrollspy({
+        target: '#mainNav',
+        offset: Number(topParameter)
+    });
+});
+
+
+new Vue({
+    el: '.container',
+    data: {},
+    methods: {
+
+    },
+    created() {
+        //console.log("test");
+    }
 });
