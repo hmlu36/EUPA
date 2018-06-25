6f45820bdfa2b802a285809e1cd6f6e2564d37c2
Joshua <hmlu36@gmail.com>
2018-06-25 18:08:38 +0800

    commit

--- vendor/vue/custom/vue-post.js
+++ vendor/vue/custom/vue-post.js
@@ -1,60 +1,67 @@
 ﻿Vue.component("v-post", {
     props: ['post'],
     template: [
-        "<div class='col-lg-4 col-sm-6 text-center mb-4'>                                 ",
-        "     <div class='thumbnail'>                                                     ",
-        "         <img class='img-fluid d-block mx-auto' v-bind:src='post.path' v-on:click='popMessage(post.mold)' alt=' '>  ",
-        "     </div>                                                                      ",
-        "     <p>{{post.title}}</p>                                                     ",
-        "     <p style='white-space: pre'>{{post.content}}</p>                            ",
-        "</div>                                                                           "
+        "<div class='col-lg-4 col-sm-6 text-center mb-4'>                                                                          ",
+        "     <div class='thumbnail'>                                                                                              ",
+        "         <img class='img-fluid d-block mx-auto col-6' v-bind:src='post.path' v-on:click='popMessage(post.mold)' alt=' '>  ",
+        "     </div>                                                                                                               ",
+        "     <p>{{post.title}}</p>                                                                                                ",
+        "     <p style='white-space: pre'>{{post.content}}</p>                                                                     ",
+        "</div>                                                                                                                    "
     ].join(""),
     methods: {
         popMessage(mold) {
 
             let $element =
-                '<div class="article-read text-left">                                                        ' +
-                '    <div class="article-read-inner">                                                        ' +
-                '        <div class="article-back">                                                          ' +
-                '            <a class="btn btn-outline-primary"><i class="ion ion-chevron-left"></i> Back</a>' +
-                '        </div>                                                                              ' +
-                ' <div class="tab" role="tabpanel">                                                                                                    ' +
-                ' <!-- Nav tabs -->                                                                                                                    ' +
-                ' <ul class="nav nav-tabs" role="tablist">                                                                                             ' +
-                ' 	<li role="presentation"><a href="#Section1" aria-controls="home" role="tab" data-toggle="tab">功能</a></li>                   ' +
-                ' 	<li role="presentation"><a href="#Section2" aria-controls="profile" role="tab" data-toggle="tab">食譜</a></li>                ' +
-                ' 	<li role="presentation"><a href="#Section3" aria-controls="messages" role="tab" data-toggle="tab">設計</a></li>               ' +
-                ' 	<li role="presentation"><a href="#Section4" aria-controls="messages" role="tab" data-toggle="tab">故事</a></li>               ' +
-                ' 	<li role="presentation"><a href="#Section5" aria-controls="messages" role="tab" data-toggle="tab">規格</a></li>               ' +
-                ' </ul>                                                                                                                                ' +
-                ' <!-- Tab panes -->                                                                                                                   ' +
-                ' <div class="tab-content tabs">                                                                                                       ' +
-                ' 	<div role="tabpanel" class="tab-pane fade in active" id="Section1">                                                                ' +
-                '        <h1 class="article-title">{title}</h1>                                              ' +
-                '        <img src="{path}">                                                                  ' +
-                '        <div class="article-content">                                                       ' +
-                '        　　　　<p style="white-space: pre">{content}</p>                                    ' +
-                '        </div>                                                                              ' +
-                ' 	</div>                                                                                                                             ' +
-                ' 	<div role="tabpanel" class="tab-pane fade" id="Section2">                                                                          ' +
-                ' 		<h3>Section 2</h3>                                                                                                             ' +
-                ' 		<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras nec urna aliquam, ornare eros vel, malesuada lorem. Nullam faucibus lorem at eros consectetur lobortis. Maecenas nec nibh congue, placerat sem id, rutrum velit. Phasellus porta enim at facilisis condimentum. Maecenas pharetra dolor vel elit tempor pellentesque sed sed eros. Aenean vitae mauris tincidunt, imperdiet orci semper, rhoncus ligula. Vivamus scelerisque.</p>' +
-                ' 	</div>                                                                                                                             ' +
-                ' 	<div role="tabpanel" class="tab-pane fade" id="Section3">                                                                          ' +
-                ' 		<h3>Section 3</h3>                                                                                                             ' +
-                ' 		<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras nec urna aliquam, ornare eros vel, malesuada lorem. Nullam faucibus lorem at eros consectetur lobortis. Maecenas nec nibh congue, placerat sem id, rutrum velit. Phasellus porta enim at facilisis condimentum. Maecenas pharetra dolor vel elit tempor pellentesque sed sed eros. Aenean vitae mauris tincidunt, imperdiet orci semper, rhoncus ligula. Vivamus scelerisque.</p>' +
-                ' 	</div>                                                                          ' +
-                ' 	<div role="tabpanel" class="tab-pane fade" id="Section4">                                                                          ' +
-                ' 		<h3>Section 4</h3>                                                                          ' +
-                ' 		<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras nec urna aliquam, ornare eros vel, malesuada lorem. Nullam faucibus lorem at eros consectetur lobortis. Maecenas nec nibh congue, placerat sem id, rutrum velit. Phasellus porta enim at facilisis condimentum. Maecenas pharetra dolor vel elit tempor pellentesque sed sed eros. Aenean vitae mauris tincidunt, imperdiet orci semper, rhoncus ligula. Vivamus scelerisque.</p>' +
-                ' 	</div>                                                                          ' +
-                ' 	<div role="tabpanel" class="tab-pane fade" id="Section5">                                                                          ' +
-                ' 		<h3>Section 5</h3>                                                                          ' +
-                ' 		<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras nec urna aliquam, ornare eros vel, malesuada lorem. Nullam faucibus lorem at eros consectetur lobortis. Maecenas nec nibh congue, placerat sem id, rutrum velit. Phasellus porta enim at facilisis condimentum. Maecenas pharetra dolor vel elit tempor pellentesque sed sed eros. Aenean vitae mauris tincidunt, imperdiet orci semper, rhoncus ligula. Vivamus scelerisque.</p>' +
-                ' 	</div>                                                                          ' +
-
+                '<div class="article-read text-left">                                                                                               ' +
+                '    <div class="article-read-inner">                                                                                               ' +
+                '        <div class="article-back">                                                                                                 ' +
+                '            <a class="btn btn-outline-primary"><i class="ion ion-chevron-left"></i> Back</a>                                       ' +
+                '        </div>                                                                                                                     ' +
+                ' <div class="tab">                                                                                                                 ' +
+                ' <ul class="nav nav-tabs" role="tablist">                                                                                          ' +
+                '     <li role="presentation"><a href="#Section1" role="tab" data-toggle="tab">功能</a></li>                                        ' +
+                '     <li role="presentation"><a href="#Section2" role="tab" data-toggle="tab">食譜</a></li>                                        ' +
+                '     <li role="presentation"><a href="#Section3" role="tab" data-toggle="tab">設計</a></li>                                        ' +
+                '     <li role="presentation"><a href="#Section4" role="tab" data-toggle="tab">故事</a></li>                                        ' +
+                '     <li role="presentation"><a href="#Section5" role="tab" data-toggle="tab">規格</a></li>                                        ' +
+                ' </ul>                                                                                                                             ' +
+                ' <div class="tab-content tabs">                                                                                                    ' +
+                '     <div class="tab-pane" id="Section1">                                                                                          ' +
+                '         <div class="row text-center">                                                                                             ' +
+                '             <div class="col-lg-4 col-sm-6 mb-3 justify-content-center">                                                           ' +
+                '                 <img src="{path}" class="col-6">                                                                                  ' +
+                '                 <p><strong>{title}</strong></p>                                                                                   ' +
+                '             </div>                                                                                                                ' +
+                '             <div class="col-6">                                                                                                   ' +
+                '          　　　　<p style="white-space: pre">{content}</p>                                                                         ' +
+                '             </div>                                                                                                                ' +
+                '         </div>                                                                                                                    ' +
+                '     </div>                                                                                                                        ' +
+                '     <div class="tab-pane" id="Section2">                                                                                          ' +
+                '         <div class="row">                                                                                                         ' +
+                '             <div class="col-lg-4 col-sm-6 mb-3 justify-content-center text-center">                                                           ' +
+                '                 <img src="{recipePath}" class="col-8">                                                                            ' +
+                '                 <p ><strong>{recipeTitle}</strong></p>                                                                             ' +
+                '             </div>                                                                                                                ' +
+                '             <div class="col-6">                                                                                                   ' +
+                '          　　　　<p style="white-space: pre">{recipeContent}</p>                                                                   ' +
+                '             </div>                                                                                                                ' +
+                '         </div>                                                                                                                    ' +
+                '     </div>                                                                                                                             ' +
+                '     <div class="tab-pane" id="Section3">                                                                          ' +
+                '         <h3>Section 3</h3>                                                                                                             ' +
+                '         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras nec urna aliquam, ornare eros vel, malesuada lorem. Nullam faucibus lorem at eros consectetur lobortis. Maecenas nec nibh congue, placerat sem id, rutrum velit. Phasellus porta enim at facilisis condimentum. Maecenas pharetra dolor vel elit tempor pellentesque sed sed eros. Aenean vitae mauris tincidunt, imperdiet orci semper, rhoncus ligula. Vivamus scelerisque.</p>' +
+                '     </div>                                                                          ' +
+                '     <div class="tab-pane" id="Section4">                                                                          ' +
+                '         <h3>Section 4</h3>                                                                          ' +
+                '         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras nec urna aliquam, ornare eros vel, malesuada lorem. Nullam faucibus lorem at eros consectetur lobortis. Maecenas nec nibh congue, placerat sem id, rutrum velit. Phasellus porta enim at facilisis condimentum. Maecenas pharetra dolor vel elit tempor pellentesque sed sed eros. Aenean vitae mauris tincidunt, imperdiet orci semper, rhoncus ligula. Vivamus scelerisque.</p>' +
+                '     </div>                                                                          ' +
+                '     <div class="tab-pane" id="Section5">                                                                          ' +
+                '         <h3>Section 5</h3>                                                                          ' +
+                '         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras nec urna aliquam, ornare eros vel, malesuada lorem. Nullam faucibus lorem at eros consectetur lobortis. Maecenas nec nibh congue, placerat sem id, rutrum velit. Phasellus porta enim at facilisis condimentum. Maecenas pharetra dolor vel elit tempor pellentesque sed sed eros. Aenean vitae mauris tincidunt, imperdiet orci semper, rhoncus ligula. Vivamus scelerisque.</p>' +
+                '     </div>                                                                          ' +
                 ' </div>                                                                            ' +
-
                 '    </div>                                                                                  ' +
                 '</div>                                                                                      ';
             var url = "mock/" + mold + ".json";
@@ -95,5 +102,8 @@
                 }
             });
         }
+    },
+    created() {
+
     }
 });
