5d25ede8d468c2d1b0e9f26a99cea7f3ce648b12
Joshua <hmlu36@gmail.com>
2018-06-26 17:56:33 +0800

    commit

--- vendor/vue/custom/vue-post.js
+++ vendor/vue/custom/vue-post.js
@@ -13,57 +13,81 @@
         popMessage(mold) {
 
             let $element =
-                '<div class="article-read text-left">                                                                                               ' +
-                '    <div class="article-read-inner">                                                                                               ' +
-                '        <div class="article-back">                                                                                                 ' +
-                '            <a class="btn btn-outline-primary"><i class="ion ion-chevron-left"></i> Back</a>                                       ' +
-                '        </div>                                                                                                                     ' +
-                ' <div class="tab">                                                                                                                 ' +
-                ' <ul class="nav nav-tabs" role="tablist">                                                                                          ' +
-                '     <li role="presentation"><a href="#Section1" role="tab" data-toggle="tab">功能</a></li>                                        ' +
-                '     <li role="presentation"><a href="#Section2" role="tab" data-toggle="tab">食譜</a></li>                                        ' +
-                '     <li role="presentation"><a href="#Section3" role="tab" data-toggle="tab">設計</a></li>                                        ' +
-                '     <li role="presentation"><a href="#Section4" role="tab" data-toggle="tab">故事</a></li>                                        ' +
-                '     <li role="presentation"><a href="#Section5" role="tab" data-toggle="tab">規格</a></li>                                        ' +
-                ' </ul>                                                                                                                             ' +
-                ' <div class="tab-content tabs">                                                                                                    ' +
-                '     <div class="tab-pane" id="Section1">                                                                                          ' +
-                '         <div class="row text-center">                                                                                             ' +
-                '             <div class="col-lg-4 col-sm-6 mb-3 justify-content-center">                                                           ' +
-                '                 <img src="{path}" class="col-6">                                                                                  ' +
-                '                 <p><strong>{title}</strong></p>                                                                                   ' +
-                '             </div>                                                                                                                ' +
-                '             <div class="col-6">                                                                                                   ' +
-                '          　　　　<p style="white-space: pre">{content}</p>                                                                         ' +
-                '             </div>                                                                                                                ' +
-                '         </div>                                                                                                                    ' +
-                '     </div>                                                                                                                        ' +
-                '     <div class="tab-pane" id="Section2">                                                                                          ' +
-                '         <div class="row">                                                                                                         ' +
-                '             <div class="col-lg-4 col-sm-6 mb-3 justify-content-center text-center">                                                           ' +
-                '                 <img src="{recipePath}" class="col-8">                                                                            ' +
-                '                 <p ><strong>{recipeTitle}</strong></p>                                                                             ' +
-                '             </div>                                                                                                                ' +
-                '             <div class="col-6">                                                                                                   ' +
-                '          　　　　<p style="white-space: pre">{recipeContent}</p>                                                                   ' +
-                '             </div>                                                                                                                ' +
-                '         </div>                                                                                                                    ' +
-                '     </div>                                                                                                                             ' +
-                '     <div class="tab-pane" id="Section3">                                                                          ' +
-                '         <h3>Section 3</h3>                                                                                                             ' +
-                '         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras nec urna aliquam, ornare eros vel, malesuada lorem. Nullam faucibus lorem at eros consectetur lobortis. Maecenas nec nibh congue, placerat sem id, rutrum velit. Phasellus porta enim at facilisis condimentum. Maecenas pharetra dolor vel elit tempor pellentesque sed sed eros. Aenean vitae mauris tincidunt, imperdiet orci semper, rhoncus ligula. Vivamus scelerisque.</p>' +
-                '     </div>                                                                          ' +
-                '     <div class="tab-pane" id="Section4">                                                                          ' +
-                '         <h3>Section 4</h3>                                                                          ' +
-                '         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras nec urna aliquam, ornare eros vel, malesuada lorem. Nullam faucibus lorem at eros consectetur lobortis. Maecenas nec nibh congue, placerat sem id, rutrum velit. Phasellus porta enim at facilisis condimentum. Maecenas pharetra dolor vel elit tempor pellentesque sed sed eros. Aenean vitae mauris tincidunt, imperdiet orci semper, rhoncus ligula. Vivamus scelerisque.</p>' +
-                '     </div>                                                                          ' +
-                '     <div class="tab-pane" id="Section5">                                                                          ' +
-                '         <h3>Section 5</h3>                                                                          ' +
-                '         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras nec urna aliquam, ornare eros vel, malesuada lorem. Nullam faucibus lorem at eros consectetur lobortis. Maecenas nec nibh congue, placerat sem id, rutrum velit. Phasellus porta enim at facilisis condimentum. Maecenas pharetra dolor vel elit tempor pellentesque sed sed eros. Aenean vitae mauris tincidunt, imperdiet orci semper, rhoncus ligula. Vivamus scelerisque.</p>' +
-                '     </div>                                                                          ' +
-                ' </div>                                                                            ' +
-                '    </div>                                                                                  ' +
-                '</div>                                                                                      ';
+                '<div class="article-read text-left">                                                                       ' +
+                '    <div class="article-read-inner">                                                                       ' +
+                '        <div class="article-back">                                                                         ' +
+                '            <a class="btn btn-outline-primary"><i class="ion ion-chevron-left"></i> Back</a>               ' +
+                '        </div>                                                                                             ' +
+                '       <div class="tab">                                                                                   ' +
+                '           <div class="d-flex justify-content-center">                                                     ' +
+                '               <ul class="nav nav-tabs mb-4 justify-content-center">                                       ' +
+                '                   <li role="presentation"><a href="#Section1" role="tab" data-toggle="tab">功能</a></li>  ' +
+                '                   <li role="presentation"><a href="#Section2" role="tab" data-toggle="tab">食譜</a></li>  ' +
+                '                   <li role="presentation"><a href="#Section3" role="tab" data-toggle="tab">設計</a></li>  ' +
+                '                   <li role="presentation"><a href="#Section4" role="tab" data-toggle="tab">故事</a></li>  ' +
+                '                   <li role="presentation"><a href="#Section5" role="tab" data-toggle="tab">規格</a></li>  ' +
+                '               </ul>                                                                                       ' +
+                '           </div>                                                                                          ' +
+                '       </div>                                                                                              ' +
+                '       <div class="tab-content post-content tabs">                                                         ' +
+                '           <div class="tab-pane active show" id="Section1">                                                ' +
+                '               <div class="row">                                                                           ' +
+                '                   <div class="col-md-6 text-right">                                                       ' +
+                '                       <img class="img-fluid" src="{path}">                                                ' +
+                '                   </div>                                                                                  ' +
+                '                   <div class="col-md-6">                                                                  ' +
+                '                       <h3 class="offset-md-2">{title}</h3>                                                ' +
+                '                       <p class="offset-md-2" style="white-space: pre">{content}</p>                       ' +
+                '                   </div>                                                                                  ' +
+                '               </div>                                                                                      ' +
+                '           </div>                                                                                          ' +
+                '           <div class="tab-pane" id="Section2">                                                            ' +
+                '               <div class="row">                                                                           ' +
+                '                   <div class="col-md-6 text-right">                                                       ' +
+                '                       <img class="img-fluid" src="{recipePath}">                                          ' +
+                '                   </div>                                                                                  ' +
+                '                   <div class="col-md-6">                                                                  ' +
+                '                       <h3 class="offset-md-2">{recipeTitle}</h3>                                          ' +
+                '                       <p class="offset-md-2" style="white-space: pre">{recipeContent}</p>                 ' +
+                '                   </div>                                                                                  ' +
+                '               </div>                                                                                      ' +
+                '           </div>                                                                                          ' +
+                '           <div class="tab-pane" id="Section3">                                                            ' +
+                '               <div class="row">                                                                           ' +
+                '                   <div class="col-md-6 text-right">                                                       ' +
+                '                       <img class="img-fluid" src="{designPath}">                                          ' +
+                '                   </div>                                                                                  ' +
+                '                   <div class="col-md-6">                                                                  ' +
+                '                       <h3 class="offset-md-2">{designTitle}</h3>                                          ' +
+                '                       <p class="offset-md-2" style="white-space: pre">{designContent}</p>                 ' +
+                '                   </div>                                                                                  ' +
+                '               </div>                                                                                      ' +
+                '           </div>                                                                                          ' +
+                '           <div class="tab-pane" id="Section4">                                                            ' +
+                '               <div class="row">                                                                           ' +
+                '                   <div class="col-md-6 text-right">                                                       ' +
+                '                       <img class="img-fluid" src="{storyPath}">                                           ' +
+                '                   </div>                                                                                  ' +
+                '                   <div class="col-md-6">                                                                  ' +
+                '                       <h3 class="offset-md-2">{storyTitle}</h3>                                           ' +
+                '                       <p class="offset-md-2" style="white-space: pre">{storyContent}</p>                  ' +
+                '                   </div>                                                                                  ' +
+                '               </div>                                                                                      ' +
+                '           </div>                                                                                          ' +
+                '           <div class="tab-pane" id="Section5">                                                            ' +
+                '               <div class="row">                                                                           ' +
+                '                   <div class="col-md-6 text-right">                                                       ' +
+                '                       <img class="img-fluid" src="{specPath}">                                            ' +
+                '                   </div>                                                                                  ' +
+                '                   <div class="col-md-6">                                                                  ' +
+                '                       <h3 class="offset-md-2">{specTitle}</h3>                                            ' +
+                '                       <p class="offset-md-2" style="white-space: pre">{specContent}</p>                   ' +
+                '                   </div>                                                                                  ' +
+                '               </div>                                                                                      ' +
+                '           </div>                                                                                          ' +
+                '       </div>                                                                                              ' +
+                '    </div>                                                                                                 ' +
+                '</div>                                                                                                     ';
             var url = "mock/" + mold + ".json";
             //alert(url);
             $.ajax({
